7e657d2ec53139fe95f976e006b9554dbd1f5893
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-09-11 16:51:10 +0900

    Add floating button for invoking context menu

--- src/main/java/com/vistatec/ocelot/segment/view/SegmentView.java
+++ src/main/java/com/vistatec/ocelot/segment/view/SegmentView.java
@@ -1195,6 +1195,7 @@ public class SegmentView extends JScrollPane implements RuleListener,
 				        });
 				editingRow = row;
 				editorComponent.setFont(font);
+                editorComponent.prepareEditingUI();
                 ToolTipManager.sharedInstance().registerComponent(editorComponent);
 			}
 			eventQueue.post(new OcelotEditingEvent(
@@ -1250,6 +1251,7 @@ public class SegmentView extends JScrollPane implements RuleListener,
             // called directly in BasicTableUI.Actions.actionPerformed(). Thus
             // we do cleanup here.
             super.removeCellEditorListener(l);
+            editorComponent.closeEditingUI();
             ToolTipManager.sharedInstance().unregisterComponent(editorComponent);
         }
 	}
